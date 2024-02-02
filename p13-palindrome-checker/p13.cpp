9a401bba71e0eca19cdf7bae1eeac0cd8fd60f7e
ABHAY <abhaybalakrishnan884@gmail.com>
2024-02-02 07:03:20 +0530

    Add p13 file

---
+++ p13-palindrome-checker/p13.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool isPali(string input){
+    int len = input.length();
+    for(int i=0; i<len/2; i++){
+        if(input[i] != input[len - i - 1]){
+            return false;            
+        }
+    }
+    return true;
+}
+int main(){
+    string input;
+
+    cout<<"Enter a number or string: ";
+    getline(cin, input);
+
+    if(isPali(input)){
+        cout<<input<<" is Palindrome";
+    }
+    else{
+        cout<<input<<" is not a Palindrome";
+    }
+    return 0;
+}
