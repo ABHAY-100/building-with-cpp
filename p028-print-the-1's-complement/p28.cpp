fe7ca8925628cea04b0fca41d6be23604cd570f8
ABHAY <abhaybalakrishnan884@gmail.com>
2024-02-23 18:34:14 +0530

    Add p028

---
+++ p028-print-the-1's-complement/p28.cpp
@@ -0,0 +1,27 @@
+// C++ Program to Print the 1's Complement of an Integer
+
+#include <iostream>
+using namespace std;
+void complement(int num){
+  int rem;
+  if(num<=1){
+    cout<<!num;
+    return;
+  }
+  rem = num%2;
+  complement(num/2);
+  cout<<!rem;
+}
+int main(){
+  int num;
+  cout<<"Enter a number: ";
+  cin>>num;
+  if(num<0){
+    cout<<num<<" is not a positive number";
+  }
+  else{
+    cout<<"The 1's Complement of "<<num<<" is: ";
+    complement(num);
+  }
+  return 0;
+}
